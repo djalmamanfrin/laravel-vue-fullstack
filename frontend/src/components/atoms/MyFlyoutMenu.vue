ef7c56abad86ff593375c6f41d31ba9824adc04f
Djalma Manfrin <djalma.manfrin@gmail.com>
2025-02-14 22:25:44 -0300

    The MyFlyoutMenu component was adapted to remove the specific code of MyRecentChanges component

--- frontend/src/components/atoms/MyFlyoutMenu.vue
+++ frontend/src/components/atoms/MyFlyoutMenu.vue
@@ -1,13 +1,6 @@
 <script setup>
 import { Popover, PopoverButton, PopoverPanel } from '@headlessui/vue'
 
-const props = defineProps({
-  actions: {
-    type: Array,
-    required: true
-  },
-});
-
 </script>
 
 <template>
@@ -19,16 +12,8 @@ const props = defineProps({
     <transition enter-active-class="transition ease-out duration-200" enter-from-class="opacity-0 translate-y-1" enter-to-class="opacity-100 translate-y-0" leave-active-class="transition ease-in duration-150" leave-from-class="opacity-100 translate-y-0" leave-to-class="opacity-0 translate-y-1">
       <PopoverPanel class="absolute z-10 mt-15 flex max-w-md -translate-x-1/5 px-4">
       <div class="w-screen max-w-md flex-auto overflow-hidden rounded-3xl bg-white text-sm/6 ring-1 shadow-lg ring-gray-900/5">
-          <div class="grid grid-cols-2 divide-x divide-gray-900/5 bg-gray-50">
-            <a v-for="item in actions" :key="item.name" href="#" class="flex items-center justify-center gap-x-2.5 p-3 font-semibold text-gray-900 hover:bg-gray-100">
-              <component :is="item.icon" class="size-5 flex-none text-gray-400" aria-hidden="true" />
-              {{ item.name }}
-            </a>
-          </div>
-          <div class="max-h-[500px] overflow-y-auto my-2 px-4">
-            <slot name="main"></slot>
-          </div>
-        </div>
+        <slot name="main"></slot>
+      </div>
       </PopoverPanel>
     </transition>
   </Popover>
--- frontend/src/components/atoms/MyRecentChanges.vue
+++ frontend/src/components/atoms/MyRecentChanges.vue
@@ -43,26 +43,34 @@ const actions = [
 </script>
 
 <template>
-  <MyFlyoutMenu :actions="actions">
+  <MyFlyoutMenu>
     <template #header>
       <BellAlertIcon v-if="boardStore.unreadChangesCount" class="block text-yellow-500 size-6" aria-hidden="true" />
       <BellIcon v-else class="block size-6" aria-hidden="true" />
     </template>
     <template #main>
-      <div v-for="item in formattedChanges" :key="item.id" class="group relative flex gap-x-6 rounded-lg px-3 py-2 hover:bg-gray-50">
-        <div class="mt-1 flex size-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
-          <component :is="item.icon" class="size-6 text-gray-600 group-hover:text-indigo-600" aria-hidden="true" />
-        </div>
-        <div class="w-full">
+      <div class="grid grid-cols-2 divide-x divide-gray-900/5 bg-gray-50">
+        <a v-for="item in actions" :key="item.name" href="#" class="flex items-center justify-center gap-x-2.5 p-3 font-semibold text-gray-900 hover:bg-gray-100">
+          <component :is="item.icon" class="size-5 flex-none text-gray-400" aria-hidden="true" />
+          {{ item.name }}
+        </a>
+      </div>
+      <div class="max-h-[500px] overflow-y-auto my-2 px-4">
+        <div v-for="item in formattedChanges" :key="item.id" class="group relative flex gap-x-6 rounded-lg px-3 py-2 hover:bg-gray-50">
+          <div class="mt-1 flex size-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
+            <component :is="item.icon" class="size-6 text-gray-600 group-hover:text-indigo-600" aria-hidden="true" />
+          </div>
+          <div class="w-full">
                 <span class="font-semibold text-gray-900">
                   {{ item.name }}
                   <span class="absolute inset-0" />
                 </span>
-          <div class="w-full flex justify-between text-sm text-gray-600 pb-1">
-            <span class="font-semibold">{{ item.username }}</span>
-            <span>{{ item.changed_at }}</span>
+            <div class="w-full flex justify-between text-sm text-gray-600 pb-1">
+              <span class="font-semibold">{{ item.username }}</span>
+              <span>{{ item.changed_at }}</span>
+            </div>
+            <p v-html="item.action" class="mt-1 text-gray-600"></p>
           </div>
-          <p v-html="item.action" class="mt-1 text-gray-600"></p>
         </div>
       </div>
     </template>
